fc0607a85b9b42191f8bb73225a4b012508c4854
unknown <bhuvakrina0708@gmail.com>
2026-06-08 16:40:53 +0530

    Fix MIME types for static assets

--- server/serve.cjs
+++ server/serve.cjs
@@ -102,8 +102,29 @@ const server = http.createServer((req, res) => {
   const filePath = path.join(DIST, pathName);
 
   if (fs.existsSync(filePath) && fs.statSync(filePath).isFile()) {
-    return fs.createReadStream(filePath).pipe(res);
-  }
+  const ext = path.extname(filePath);
+
+  const mimeTypes = {
+    ".html": "text/html",
+    ".js": "application/javascript",
+    ".mjs": "application/javascript",
+    ".css": "text/css",
+    ".json": "application/json",
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+    ".jpeg": "image/jpeg",
+    ".svg": "image/svg+xml",
+    ".ico": "image/x-icon",
+    ".woff": "font/woff",
+    ".woff2": "font/woff2"
+  };
+
+  res.writeHead(200, {
+    "Content-Type": mimeTypes[ext] || "application/octet-stream"
+  });
+
+  return fs.createReadStream(filePath).pipe(res);
+}
 
   /* ---------------- SPA ---------------- */
 
